a428a04fa97d4ca587701d4f42d37a15170931b2
sani337 <masrooor.28@gmail.com>
2025-10-31 19:51:26 +0600

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,123 @@ const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll
 }, appearOptions);
 
 faders.forEach(fader=>{ appearOnScroll.observe(fader); });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const track = document.querySelector('.carousel-track');
+//   const images = document.querySelectorAll('.carousel img');
+//   let index = 0;
+//   let interval;
+
+//   function startSlide() {
+//     interval = setInterval(() => {
+//       index = (index + 1) % images.length;
+//       updateSlide();
+//     }, 2500);
+//   }
+
+//   function stopSlide() {
+//     clearInterval(interval);
+//   }
+
+//   function updateSlide() {
+//     track.style.transform = `translateX(-${index * 100}%)`;
+//   }
+
+//   // Hover pause
+//   track.addEventListener('mouseenter', stopSlide);
+//   track.addEventListener('mouseleave', startSlide);
+
+//   startSlide();
+
+//   // Modal logic
+//   const modal = document.querySelector('.modal');
+//   const modalImg = document.querySelector('.modal-img');
+//   const closeBtn = document.querySelector('.close');
+//   const leftArrow = document.querySelector('.arrow.left');
+//   const rightArrow = document.querySelector('.arrow.right');
+
+//   images.forEach((img, i) => {
+//     img.addEventListener('click', () => {
+//       modal.style.display = 'flex';
+//       modalImg.src = img.src;
+//       index = i;
+//       stopSlide();
+//     });
+//   });
+
+//   closeBtn.onclick = () => {
+//     modal.style.display = 'none';
+//     startSlide();
+//   };
+
+//   leftArrow.onclick = () => {
+//     index = (index - 1 + images.length) % images.length;
+//     modalImg.src = images[index].src;
+//   };
+
+//   rightArrow.onclick = () => {
+//     index = (index + 1) % images.length;
+//     modalImg.src = images[index].src;
+//   };
+
+//   // Click outside image to close
+//   modal.addEventListener('click', (e) => {
+//     if (e.target === modal) {
+//       modal.style.display = 'none';
+//       startSlide();
+//     }
+//   });
+
+
+
+  const modal = document.querySelector('.modal');
+  const modalImg = document.querySelector('.modal-img');
+  const closeBtn = document.querySelector('.close');
+  const leftArrow = document.querySelector('.arrow.left');
+  const rightArrow = document.querySelector('.arrow.right');
+  const images = document.querySelectorAll('.carousel img');
+  let currentIndex = 0;
+
+  // Open modal on click
+  images.forEach((img, i) => {
+    img.addEventListener('click', () => {
+      modal.style.display = 'flex';
+      modalImg.src = img.src;
+      currentIndex = i % (images.length / 2); // handle duplicated images
+    });
+  });
+
+  // Close modal
+  closeBtn.onclick = () => modal.style.display = 'none';
+  modal.onclick = (e) => { if (e.target === modal) modal.style.display = 'none'; }
+
+  // Navigate modal
+  leftArrow.onclick = () => {
+    currentIndex = (currentIndex - 1 + images.length/2) % (images.length/2);
+    modalImg.src = images[currentIndex].src;
+  };
+
+  rightArrow.onclick = () => {
+    currentIndex = (currentIndex + 1) % (images.length/2);
+    modalImg.src = images[currentIndex].src;
+  };
+
